9b9412719f71db7f022802221c1b43a5b2e0da63
stillwwater <stillwwater@gmail.com>
2020-07-08 00:38:09 -0700

    Initial commit

---
+++ precache-manifest.58d60902d85c96b1a4312c99e972ed04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f6a0ada6194760b2e154487517a9bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ace13681d5762499652b",
+    "url": "/static/css/main.6b4dc562.chunk.css"
+  },
+  {
+    "revision": "8c2cb92f06f6bbff53ca",
+    "url": "/static/js/2.87020d7c.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/static/js/2.87020d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace13681d5762499652b",
+    "url": "/static/js/main.591dffaf.chunk.js"
+  },
+  {
+    "revision": "f730f3477dc2779ee533",
+    "url": "/static/js/runtime-main.a140644f.js"
+  }
+]);
